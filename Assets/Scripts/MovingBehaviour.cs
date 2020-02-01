a6015a2f7e800d4eaee7d3d1ee970e005d610590
Louis-dev <louis.nillus@gmail.com>
2020-02-01 03:50:25 +0100

    Scriptable Architecture

---
+++ Assets/Scripts/MovingBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBehaviour : MonoBehaviour
+{
+
+    public RacerTyper racerType;
+    private Rigidbody2D rb;
+  
+
+    // Start
+    void Start()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = racerType.sprite;
+
+        if (!this.GetComponent<Rigidbody2D>())
+            Debug.LogError("Missing Rigidbody2D on this gameObject");
+
+        rb = this.GetComponent<Rigidbody2D>();
+
+        rb.mass = racerType.mass;
+        rb.gravityScale = racerType.gravity;
+    }
+
+    // Update
+    void Update()
+    {
+        
+    }
+
+    //FixedUpdate
+    void FixedUpdate()
+    {
+        //Movement();
+        MovementBis();
+    }
+
+    public void Movement()
+    {
+        switch (racerType.direction)
+        {
+            case RacerTyper.Direction.Left:
+                this.transform.Translate(Vector3.left * Time.deltaTime * racerType.forceApplied);
+                break;
+            case RacerTyper.Direction.Right:
+                this.transform.Translate(Vector3.right * Time.deltaTime * racerType.forceApplied);
+                break;
+        }
+    }
+
+    public void MovementBis()
+    {
+        switch (racerType.direction)
+        {
+            case RacerTyper.Direction.Left:
+                rb.velocity = (Vector2.left * racerType.forceApplied * Time.deltaTime);
+                break;
+            case RacerTyper.Direction.Right:
+                rb.velocity = (Vector2.right * racerType.forceApplied * Time.deltaTime);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/RacerTyper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class RacerTyper : ScriptableObject
+{
+    //public TypeOfRacer typeOfRacer;
+
+    [Header("Visual Settings")]
+    public Sprite sprite;
+
+
+    [Header("Movement Settings")]
+    public Direction direction;
+    [Range(0,300)]
+    public float forceApplied;
+
+    [Header("Physics Settings")]
+    [Range(0, 50)]
+    public float mass = 1;
+    [Range(0, 10)]
+    public float gravity = 1;
+    
+    [Header("Damages Settings")]
+    [Range(0,100)]
+    public int minDmg;
+    [Range(0,100)]
+    public int maxDmg;   
+
+    [Header("Rewards Settings")]
+    public Reward reward;
+    [Range(0, 100)]
+    public int amount;
+
+    //public enum TypeOfRacer {Caravan, Goldy, Boss}
+    public enum Direction {Left, Right}
+    public enum Reward {Wood, Iron, Nails, Money}
+}
