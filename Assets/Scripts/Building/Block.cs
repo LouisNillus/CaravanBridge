9ace1eae26a1934980ad913a2183fef242bf51a3
Gyotek <arthur.herbreteau@laposte.net>
2020-02-01 13:49:24 +0100

    Block Selection

---
+++ Assets/Scripts/Building/Block.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        ElementsPlacer.instance.actualBox = transform;
+    }
+
+    private void OnMouseExit()
+    {
+        ElementsPlacer.instance.actualBox = null;
+    }
+
+    private void OnMouseOver()
+    {
+        if (ElementsPlacer.instance.actualBox != transform)
+            ElementsPlacer.instance.actualBox = transform;
+    }
+}
---
+++ Assets/Scripts/Building/ElementsPlacer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementsPlacer : MonoBehaviour
+{
+    public static ElementsPlacer instance;
+    private void Awake() { instance = this; }
+
+    public bool isPlacing;
+    bool isFirstBlockSelected;
+    bool isSecondBlockSelected;
+    Transform firstBox;
+    Transform secondBox;
+    public Transform actualBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPlacing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPlacing && isFirstBlockSelected && !isSecondBlockSelected)
+        {
+
+        }
+        else if (isPlacing && isFirstBlockSelected && isSecondBlockSelected)
+        {
+            isPlacing = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (isPlacing && !isFirstBlockSelected)
+        {
+            firstBox = actualBox;
+            isFirstBlockSelected = true;
+        }
+    }
+    private void OnMouseUp()
+    {
+        if (isPlacing && isFirstBlockSelected && !isSecondBlockSelected)
+        {
+            secondBox = actualBox;
+            isSecondBlockSelected = true;
+        }
+    }
+}
